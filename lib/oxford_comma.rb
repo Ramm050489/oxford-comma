5232d44e6712bfcd854d44f682744b4fadf6989f
Ramon <ramm050489@gmail.com>
2020-12-16 13:42:41 -0500

    commit tree 6c962bef8b77d1a34ffcd15208dc853ee1de6d07

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        array.join(" and ")
+    else 
+        pop = array.pop
+        array << "and #{pop}"
+        array.join(", ")
+    end
+    #"#{array}" did not work
+    
 end
